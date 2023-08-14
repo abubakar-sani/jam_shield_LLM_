0564dc0626c65526c0f98988d644750381491375
asataur <engrabubakarsani@gmail.com>
2023-08-14 20:01:19 +0100

    Moving the model to main()

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ from tester import test
 import transformers
 from transformers import TFAutoModelForCausalLM, AutoTokenizer
 
-# Move the transformers related setup outside the Streamlit app's main function
-model_name = "tiiuae/falcon-7b-instruct"
-model = TFAutoModelForCausalLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-pipeline = transformers.pipeline("text-generation", model=model, tokenizer=tokenizer, max_length=100, temperature=0.7)
-
 
 def main():
+    model_name = "tiiuae/falcon-7b-instruct"
+    model = TFAutoModelForCausalLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    pipeline = transformers.pipeline("text-generation", model=model, tokenizer=tokenizer, max_length=100,
+                                     temperature=0.7)
+
     st.title("Beyond the Anti-Jam: Integration of DRL with LLM")
 
     st.sidebar.header("Make Your Environment Configuration")
