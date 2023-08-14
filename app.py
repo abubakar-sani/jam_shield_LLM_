5acfeb6a7f19ddc1b5f6815256c4010b08798b2d
asataur <engrabubakarsani@gmail.com>
2023-08-14 11:50:16 +0100

    UI update

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 import streamlit as st
 
 def main():
-    st.title("Anti-Jamming Configuration App")
+    st.title("Beyond the Anti-Jam: Integration of DRL with LLM")
+    st.header("Make Your Environment Configuration")
     mode = st.radio("Choose Mode", ["Auto", "Manual"])
 
     if mode == "Auto":
         jammer_type = "dynamic"
-        agent_type = "DQN with prioritized replay memory"
         channel_switching_cost = 0.1
     else:
         jammer_type = st.selectbox("Select Jammer Type", ["constant", "sweeping", "random", "dynamic"])
-        agent_type = st.selectbox("Select Agent Type", ["DQN", "DQN with fixed targets", "DDQN", "Dueling DDQN", "DQN with prioritized replay memory"])
         channel_switching_cost = st.selectbox("Select Channel Switching Cost", [0, 0.05, 0.1, 0.15, 0.2])
 
-    st.write("Configuration:")
+    st.subheader("Configuration:")
     st.write(f"Jammer Type: {jammer_type}")
-    st.write(f"Agent Type: {agent_type}")
     st.write(f"Channel Switching Cost: {channel_switching_cost}")
 
     st.write("==================================================")
