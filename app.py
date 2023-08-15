12f69dd4c55747309e2f1897672c8aacd0aed773
asataur <engrabubakarsani@gmail.com>
2023-08-15 07:42:02 +0100

    Integrating the DRL and LLM

--- app.py
+++ app.py
@@ -10,6 +10,30 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
 
+model = "tiiuae/falcon-7b-instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto",
+)
+sequences = pipeline(
+   "Girafatron is obsessed with giraffes, the most glorious animal on the face of this Earth. Giraftron believes all other animals are irrelevant when compared to the glorious majesty of the giraffe.\nDaniel: Hello, Girafatron!\nGirafatron:",
+    max_length=200,
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+)
+st.title("Beyond the Anti-Jam: Integration of DRL with LLM")
+for seq in sequences:
+    st.write(f"Result: {seq['generated_text']}")
+
+
 def perform_training(jammer_type, channel_switching_cost):
     agent = train(jammer_type, channel_switching_cost)
     return agent
@@ -19,13 +43,13 @@ def perform_testing(agent, jammer_type, channel_switching_cost):
     test(agent, jammer_type, channel_switching_cost)
 
 
-model_name = "tiiuae/falcon-7b-instruct"
-model = AutoModelForCausalLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-pipeline = transformers.pipeline("text-generation", model=model, tokenizer=tokenizer, max_length=100,
-                                 temperature=0.7)
+# model_name = "tiiuae/falcon-7b-instruct"
+# model = AutoModelForCausalLM.from_pretrained(model_name)
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# pipeline = transformers.pipeline("text-generation", model=model, tokenizer=tokenizer, max_length=100,
+#                                  temperature=0.7)
 
-st.title("Beyond the Anti-Jam: Integration of DRL with LLM")
+# st.title("Beyond the Anti-Jam: Integration of DRL with LLM")
 
 st.sidebar.header("Make Your Environment Configuration")
 mode = st.sidebar.radio("Choose Mode", ["Auto", "Manual"])
@@ -46,6 +70,6 @@ start_button = st.sidebar.button('Start')
 if start_button:
     agent = perform_training(jammer_type, channel_switching_cost)
     st.subheader("Generating Insights of the DRL-Training")
-    text = pipeline("Discuss this topic: Integrating LLMs to DRL-based anti-jamming.")
-    st.write(text)
+    # text = pipeline("Discuss this topic: Integrating LLMs to DRL-based anti-jamming.")
+    # st.write(text)
     test(agent, jammer_type, channel_switching_cost)
